cc3eaf4d187c7d3f4e4fbbe7b42f2d4fafe96198
VadimCurca <vadim.curca14@gmail.com>
2022-06-01 18:08:00 +0300

    add subreddit classification

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ from karateclub.dataset import GraphSetReader
 from karateclub import FeatherGraph
 from sklearn.model_selection import train_test_split
 import networkx as nx
+from karateclub import FeatherGraph
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 import itertools
+import copy
 
 from sklearn.metrics import roc_auc_score
 from sklearn.linear_model import LogisticRegression
@@ -61,11 +65,6 @@ def col_content(key):
     if not cached_graphs:
         number_posts_to_load = st.number_input('Number of posts to load', int(5), key=key)
 
-    if first_run_str not in st.session_state:
-        st.session_state[first_run_str] = True
-    if graph_str not in st.session_state:
-        st.session_state[graph_str] = []
-
     if st.button('Load graphs', key=key):
         st.session_state[first_run_str] = False
 
@@ -103,6 +102,41 @@ def col_content(key):
         st.table(df)
 
 
+def compute_graphs_embedding_and_train():
+    graph1_orig, _ = st.session_state["graph1"]
+    graph2_orig, _ = st.session_state["graph2"]
+
+    graph1 = copy.deepcopy(graph1_orig)
+    graph2 = copy.deepcopy(graph2_orig.copy())
+
+    graphs = graph1 + graph2
+    targets = [0]*len(graph1) + [1]*len(graph2)
+
+    model = FeatherGraph()
+    model.fit(graphs)
+    X = model.get_embedding()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, targets, test_size=0.2)
+
+    downstream_model = LogisticRegression(max_iter=500).fit(X_train, y_train)
+
+    st.session_state["model"] = model
+    st.session_state["downstream_model"] = downstream_model
+    st.session_state["downstream_model_score"] = downstream_model.score(X_test, y_test)
+
+
+graph_str_list = ["graph1", "graph2"]
+
+if "first_run1" not in st.session_state:
+    st.session_state["first_run1" ] = True
+if "first_run2" not in st.session_state:
+    st.session_state["first_run2" ] = True
+
+for graph_str in graph_str_list:
+    if graph_str not in st.session_state:
+        st.session_state[graph_str] = []
+
+
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
@@ -111,3 +145,21 @@ with col1:
 
 with col2:
     col_content(2)
+
+if st.session_state["first_run1"] == False and st.session_state["first_run2"] == False:
+    st.markdown("---")
+    st.markdown("## Graphs classification")
+
+
+    if st.button("Compute graphs embedding and train"):
+        compute_graphs_embedding_and_train()
+    
+    if "downstream_model" in st.session_state:
+        score = st.session_state["downstream_model_score"]
+        st.write("test score: ", score)
+
+
+
+
+
+        
